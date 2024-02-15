384c597f64785104bbc66182680107289eb07c25
ignaciofs87 <igfernan@Mac-mini-de-Ignacio.local>
2024-02-15 18:57:50 +0100

    Add UserDto.Java

--- src/main/java/com/nacho/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/nacho/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nacho.curso.springboot.webapp.springbootweb.models.User;
+import com.nacho.curso.springboot.webapp.springbootweb.models.dto.UserDto;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/details-rest")
-    public Map<String, Object> details() {
+    @GetMapping("/details-map")
+    public Map<String, Object> detailsMap() {
         User user = new User("Ignacio", "Fernandez Sanchez");
         Map<String, Object> body = new HashMap<String, Object>();
 
@@ -22,6 +23,14 @@ public class UserRestController {
         body.put("user", user);
 
         return body;
+    }
+
+    @GetMapping("/details-dto")
+    public UserDto detailsDto() {
+        User user = new User("Ignacio", "Fernandez Sanchez");
+
+        UserDto userDto = new UserDto("Hola Mundo Spring Boot", user.getName(), user.getLastname());
+        return userDto;
     }
 
 }
