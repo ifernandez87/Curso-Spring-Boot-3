dcb6a5b5991efd4a312b288c9a403a3f62e46062
ignaciofs87 <igfernan@Mac-mini-de-Ignacio.local>
2024-02-25 00:36:47 +0100

    update seccion 3

---
+++ seccion3/src/test/java/es/spring/boot3/seccion3/Seccion3ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.spring.boot3.seccion3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seccion3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
