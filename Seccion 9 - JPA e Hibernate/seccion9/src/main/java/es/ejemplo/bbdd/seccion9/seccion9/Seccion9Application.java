0993ba4b8b90543ac74eaec021541071293a8af3
Ignacio Fernandez Sanchez <ignaciofs87@gmail.com>
2024-03-03 23:06:42 +0100

    Update Seccion 9

---
+++ Seccion 9 - JPA e Hibernate/seccion9/src/main/java/es/ejemplo/bbdd/seccion9/seccion9/Seccion9Application.java
@@ -0,0 +1,13 @@
+package es.ejemplo.bbdd.seccion9.seccion9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seccion9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seccion9Application.class, args);
+	}
+
+}
---
+++ Seccion 9 - JPA e Hibernate/seccion9/src/test/java/es/ejemplo/bbdd/seccion9/seccion9/Seccion9ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ejemplo.bbdd.seccion9.seccion9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seccion9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
